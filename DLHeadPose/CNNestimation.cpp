32a233ef07ac3640a0cbd8b976999829a8f19c75
Xingyuan Bu <buyuantb@163.com>
2015-11-03 21:27:55 +0800

    DLHeadPose for video frame

--- DLHeadPose/CNNestimation.cpp
+++ DLHeadPose/CNNestimation.cpp
@@ -3,44 +3,75 @@
 #include <opencv/highgui.h>
 #include "tiny_cnn.h"
 
+//#define VISUALIZE
+#define FOR_VIDEO
+
 using namespace tiny_cnn;
 using namespace tiny_cnn::activation;
 using namespace std;
 
-// rescale output to 0-100
-template <typename Activation>
-double rescale(double x) {
-	Activation a;
-	return 100.0 * (x - a.scale().first) / (a.scale().second - a.scale().first);
-}
+cv::VideoCapture m_videocapture = cv::VideoCapture(0);
 
 // convert tiny_cnn::image to cv::Mat and resize
-cv::Mat image2mat(image<>& img) {
+cv::Mat Image2Mat(image<>& img) {
 	cv::Mat ori(img.height(), img.width(), CV_8U, &img.at(0, 0));
 	cv::Mat resized;
 	cv::resize(ori, resized, cv::Size(), 3, 3, cv::INTER_AREA);
 	return resized;
 }
 
-void convert_image(const std::string& imagefilename,
-	double minv,
-	double maxv,
+int ResizeImage(cv::Mat img, double minv, double maxv,
 	int w,
 	int h,
 	vec_t& data)
 {
-	auto img = cv::imread(imagefilename, cv::IMREAD_GRAYSCALE);
-	if (img.data == nullptr) return; // cannot open, or it's not an image
-
 	cv::Mat_<uint8_t> resized;
 	cv::resize(img, resized, cv::Size(w, h));
+	for (int i = 0; i < resized.size().width; i++)
+	{
+		for (int j = 0; j < resized.size().height; j++)
+		{
+			//Task
+			double temp = (255 - resized[i][j]) * (maxv - minv) / 255.0 + minv;
+			data.push_back(temp);
+		}
+	}
 
-	// mnist dataset is "white on black", so negate required
-	std::transform(resized.begin(), resized.end(), std::back_inserter(data),
-		[=](uint8_t c) { return (255 - c) * (maxv - minv) / 255.0 + minv; });
+	return 0;
 }
 
-void recognize(const std::string& dictionary, const std::string& filename) {
+int GetImageDataFromVideo(vec_t& data)
+{
+	cv::Mat frame;
+	m_videocapture >> frame;
+	// cannot open, or it's not an image
+	if (frame.data == nullptr)
+	{
+		return 0;
+	}
+	//convert a frame into gray
+	cv::Mat cvtBGRimg;
+	cv::cvtColor(frame, cvtBGRimg, CV_BGR2GRAY);
+
+	//resize a image 
+	ResizeImage(cvtBGRimg,- 1.0, 1.0, 32, 32, data);
+
+	return 0;
+}
+
+int GetImageDataFromPicture(const std::string &picutrefilename, vec_t& data)
+{
+	auto img = cv::imread(picutrefilename, cv::IMREAD_GRAYSCALE);
+	// cannot open, or it's not an image
+	if (img.data == nullptr)
+	{
+		return 0;
+	}
+	ResizeImage(img, -1.0, 1.0, 32, 32, data);
+	return 0;
+}
+
+int Recognize(const std::string& dictionary) {
 	network<mse, adagrad> nn; // specify loss-function and learning strategy
 	nn << convolutional_layer<tan_h>(32, 32, 5, 1, 6)
 		<< average_pooling_layer<tan_h>(28, 28, 6, 2)
@@ -48,55 +79,73 @@ void recognize(const std::string& dictionary, const std::string& filename) {
 		<< average_pooling_layer<tan_h>(10, 10, 16, 2)
 		<< convolutional_layer<tan_h>(5, 5, 5, 16, 120)
 		<< fully_connected_layer<tan_h>(120, 10);
+
 	// load nets
-	// change all layers at once
-	nn[0]->weight_init(weight_init::xavier(4.0));
-	nn[0]->bias_init(weight_init::constant(1.0));
 	ifstream ifs(dictionary.c_str());
 	ifs >> nn;
-	for (int i = 0; i < nn.depth(); i++)
-	{
-		vec_t& my_weight = nn[i]->weight();
-		
-		cout << "layer :" << i << endl << my_weight.size() << endl;
-		for (int j = 0; j < my_weight.size();j++)
-		{
-			//cout << my_weight[j] << endl;
-			my_weight[j] = i * 2;
-		}
-		vec_t& my_bias = nn[i]->bias();
-		cout << my_bias.size() << endl<<endl;
-		for (int j = 0; j < my_bias.size(); j++)
-		{
-			//cout << my_bias[j] << endl;
-			my_bias[j] = i * 2 + 1;
-		}
-	}
-	ofstream ofs("xbu-weights");
-	ofs << nn;
-	// convert imagefile to vec_t
+
 	vec_t data;
-	convert_image(filename, -1.0, 1.0, 32, 32, data);
+	///////////////for video/////////////////////////////
+#ifdef FOR_VIDEO
+	while (1)
+	{
+		vec_t().swap(data);
+		GetImageDataFromVideo(data);
+		
+		// recognize
+		auto res = nn.predict(data);
+		vector<pair<double, int> > scores;
 
+		// sort & print top-3
+		for (int i = 0; i < res.size(); i++)
+			scores.emplace_back(res[i], i);
+
+		sort(scores.begin(), scores.end(), greater<pair<double, int>>());
+
+		cout << scores[0].second << "," << scores[0].first << endl;
+	}
+#endif
+
+	///////////////for picture//////////////////////////////
+#ifndef FOR_VIDEO
+	GetImageDataFromPicture("4.bmp", data);
 	// recognize
+	auto res = nn.predict(data);
+	vector<pair<double, int> > scores;
 
+	// sort & print top-3
+	for (int i = 0; i < res.size(); i++)
+		scores.emplace_back(res[i], i);
+
+	sort(scores.begin(), scores.end(), greater<pair<double, int>>());
+
+	for (int i = 0; i < scores.size() / 2; i++)
+	{
+		cout << scores[i].second << "," << scores[i].first << endl;
+	}
+
+	////////////////////visualize//////////////////////////////////
+#ifdef VISUALIZE
 	// visualize outputs of each layer
 	for (size_t i = 0; i < nn.depth(); i++) {
-		auto out_img = nn[i]->output_to_image();
-		cv::imshow("layer:" + std::to_string(i), image2mat(out_img));
+		auto out_img = nn[i]->output_to_image(); 
+		cv::imshow("layer:" + std::to_string(i), Image2Mat(out_img));
 	}
 	// visualize filter shape of first convolutional layer
 	auto weight = nn.at<convolutional_layer<tan_h>>(0).weight_to_image();
-	cv::imshow("weights:", image2mat(weight));
+	cv::imshow("weights:", Image2Mat(weight));
+#endif
 
 	cv::waitKey(0);
+#endif
+	return 0;
 }
 
 int main(int argc, char** argv) {
 	if (argc != 2) {
-		cout << "please specify image file" << endl;
+		cout << "please specify image file" << endl;;
 		//return 0;
 	}
-	//recognize("LeNet-weights", "4.bmp");
-	recognize("xbu-weights", "4.bmp");
+	Recognize("LeNet-weights");
+	return 0;
 }
